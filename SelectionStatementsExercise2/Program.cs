7a47659bd51d6b5caaebb9656fc910a3b3a3db3c
kendall chapman <jkc4310@gmail.com>
2023-07-07 21:06:47 -0400

    Selection Statements Exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello,what is your favorite subject in school?");
+
+            var Subject = Console.ReadLine();
+
+            switch (Subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is very challenging!");
+                    break;
+
+                case "science":
+                    Console.WriteLine("You must be very smart to enjoy science!");
+                    break;
+
+                case "history":
+                    Console.WriteLine("It's always interesting to study how we got here!");
+                    break;
+
+                case "gym":
+                    Console.WriteLine("You must be an athlete!");
+                    break;
+
+                case "spanish":
+                    Console.WriteLine("Being bilingual is a great skill to have!");
+                    break;
+
+                default:
+                    Console.WriteLine("Hmm, I haven't hear of that subject!");
+                    break;
+            }
         }
     }
 }
